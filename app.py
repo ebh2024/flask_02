c55e4967d9d672f969c337c1f29189303520d1bd
Ezequiel Busto Hurtado <e.bustohurtado@itecriocuarto.org.ar>
2024-06-25 20:32:27 -0300

    actualiza app

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+#haciendos cambios
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/Vehiculo_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
